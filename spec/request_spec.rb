d7c753d7b2f624af5fdd0e9c2a33fc80e1fc0bac
Ivan Reshetnikov <resivalex@gmail.com>
2018-04-01 21:39:03 +0300

    Fix get parameters

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -15,11 +15,19 @@ describe MyTargetApi::Request do
 
   describe 'request something' do
     it 'return parsed json' do
-      stub_request(:get, 'https://target.my.com/api/v1/campaigns.json')
+      stub_request(:post, 'https://target.my.com/api/v1/campaigns.json')
         .to_return(body: '{"name": "Campaign 1"}')
 
-      expect(subject.get('https://target.my.com/api/v1/campaigns.json'))
+      expect(subject.post('https://target.my.com/api/v1/campaigns.json'))
         .to eq('name' => 'Campaign 1')
+    end
+
+    it 'pass parameters by url in get' do
+      stub_request(:get, 'https://target.my.com/api/v1/vk_groups.json?q=unfound')
+        .to_return(body: '[]')
+
+      expect(subject.get('https://target.my.com/api/v1/vk_groups.json', q: 'unfound'))
+        .to eq([])
     end
   end
 end
