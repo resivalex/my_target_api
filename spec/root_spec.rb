18ead9d27cb12d7ea2e329c0ff9066f57d838480
Ivan Reshetnikov <resivalex@gmail.com>
2020-02-28 14:14:24 +0300

    Rollback test for headers

--- spec/root_spec.rb
+++ spec/root_spec.rb
@@ -4,9 +4,8 @@ require 'my_target_api'
 
 describe MyTargetApi do
   let(:access_token) { 'Access token' }
-  let(:headers) { { accept_language: 'ru-RU,ru' } }
 
-  subject { MyTargetApi.new(access_token, headers) }
+  subject { MyTargetApi.new(access_token) }
 
   it { should respond_to :resource }
 
